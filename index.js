607b74d4ffce8f088215d5c83a326f60fab0172f
SebasPask <sebaspask@gmail.com>
2017-10-10 16:07:01 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+app.on('ready',() => {
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
